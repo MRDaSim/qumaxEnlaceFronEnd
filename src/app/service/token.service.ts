8fce11734ffef3cfdafb9d2e1d707857684a054e
MRDaSim <danialbrecht9@gmail.com>
2022-12-26 22:03:50 -0300

    toekn service al 50%

--- src/app/service/token.service.ts
+++ src/app/service/token.service.ts
@@ -17,24 +17,37 @@ export class TokenService {
 
   public onToken() : Observable<any> {
 
-    let token: Token;
+    
 
     let body = new HttpParams();
-    body.set('grant_type', '{{credentials}}' );
+    body.set('grant_type', 'client_credentials' );
     body.set('client_id', 'soporte@qumax.com.ar');
     body.set('client_secret', 'Qumax1234');
     
     let options = {
         headers: new HttpHeaders()
         .set('Content-Type', 'application/x-www-form-urlencoded')
-        
+        .set('Access-Control-Allow-Credentials',  'true')
+        .set('Access-Control-Allow-Origin', 'http://localhost:4200')
+        //.set('Content-Length', '86')
+        //.set('Access', '*/*')
+        //.set('Accept-Encoding', 'gzip, deflate, br')
+        //.set('Access-Control-Allow-Credentials', 'true')
+        //.set('Access-Control-Allow-Origin', '*')
+        //.set('Connection', 'keep-alive')
+        //.set('User-Agent', 'PostmanRuntime/7.30.0')
+        //.set('Host', 'rest.contabilium.com')
+        //.set('Content-Length', ' ')
+        //.set('User-Agent', 'PostmanRuntime/7.30.0')
+
+
         
 
         
         
     };
     
-    return this.http.post<any>('http://rest.contabilium.com/token', body, options)
+    return this.http.post<any>('http://rest.contabilium.com/'+ 'token', body, options)
 
       
       
